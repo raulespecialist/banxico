a9397eda27475083b231049c16db9f1a66e41bac
Raul Espinosa <54911620+raulespecialist@users.noreply.github.com>
2020-12-12 14:15:20 -0600

    Update urls.py
    cambios perdidos

--- raulbanxico/urls.py
+++ raulbanxico/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from udis import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/', include('udis.urls')),
+    path('', auth_views.LoginView.as_view(template_name='index.html'), name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
